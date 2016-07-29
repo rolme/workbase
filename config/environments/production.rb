bfdc0c72234af17b277a39ec4f1582563eefeca9
Roland Parnaso <roland@sparklab.io>
2016-07-28 20:26:34 -0700

    Enable raise on email error.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   if Rails.application.secrets.enable_mailer
     config.action_mailer.smtp_settings = {
