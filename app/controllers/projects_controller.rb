3b35ecc7d0883e55d4cb3d9ca9369c06151474c2
Roland Parnaso <roland@sparklab.io>
2016-08-16 08:41:27 -0700

    Add page header and new stuff.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,12 @@ class ProjectsController < ApplicationController
     @projects = projects.sorted
   end
 
+  def new
+    client   = params[:c].present? ? Client.find(params[:c]) : Client.new
+    @project = Project.new(client_id: client.id)
+    @clients = Client.with_company_id(current_user.company_id)
+  end
+
   def show
     @project    = project
     @attachment = Attachment.new
