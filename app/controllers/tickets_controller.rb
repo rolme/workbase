5c51a54e78bff4339d7cae38b4c43ecf8cfff38c
sean linke <sean.tnl@gmail.com>
2016-08-15 23:41:31 -0700

    Ticket CRUD

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,69 @@
 class TicketsController < ApplicationController
   def index
+    @tickets = tickets.active.order(:status, :created_at)
   end
 
   def show
+    @ticket = ticket
+
+    if @ticket.unviewed? || @ticket.viewed_by.nil?
+      # Mark this ticket as viewed
+      @ticket.unassigned!
+      @ticket.viewed_by = current_user.id
+      @ticket.save
+    end
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params.merge({
+        company_id: current_user.company_id,
+        viewed_by: current_user.id,
+        created_by: current_user.id,
+    }))
+    if @ticket.save
+      flash[:success] = 'Ticket created'
+      redirect_to @ticket
+    else
+      flash[:error] = @ticket.errors.full_messages
+      render :edit
+    end
+  end
+
+  def edit
+    @ticket = ticket
+  end
+
+  def update
+    @ticket = ticket
+    if @ticket.update(ticket_params)
+      redirect_to @ticket
+    else
+      flash[:error] = @ticket.errors.full_messages
+      render 'edit'
+    end
+  end
+
+  def destroy
+    ticket.delete
+    flash[:success] = 'Ticket deleted'
+    redirect_to tickets_path
+  end
+
+private
+
+  def tickets
+    Ticket.where(company_id: current_user.company_id)
+  end
+
+  def ticket
+    tickets.find_by(slug: params[:slug])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description, :status, :assigned_to)
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,28 @@
 class Ticket < ApplicationRecord
+  include Sluggable
+  include SoftDeletable
+
   belongs_to :company
 
-  enum status: { unviewed: 0, viewed: 1, assigned: 2, closed: 9 }
+  before_save :check_status
+
+  enum status: { unviewed: 0, unassigned: 1, assigned: 2, closed: 9 }
+
+  def assignee
+    assigned_to.present? ? User.find_by(id: assigned_to) : NullUser.new
+  end
+
+private
+
+  def check_status
+    return if self.closed?
+
+    if viewed_by.nil?
+      self.status = Ticket.statuses[:unviewed]
+    elsif viewed_by.present? && assigned_to.nil?
+      self.status = Ticket.statuses[:unassigned]
+    elsif assigned_to.present?
+      self.status = Ticket.statuses[:assigned]
+    end
+  end
 end
