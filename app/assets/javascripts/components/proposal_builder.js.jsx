fa724f3ad818924ad17eabe727a96613a5022746
Roland Parnaso <roland@sparklab.io>
2016-08-23 09:23:44 -0700

    Add TODO for drag and drop using react.

--- app/assets/javascripts/components/proposal_builder.js.jsx
+++ app/assets/javascripts/components/proposal_builder.js.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// TODO: Use this for drag and drop - http://gaearon.github.io/react-dnd/
+
 export default class ProposalBuilder extends React.Component {
   render() {
     return <h1>Proposal Builder - {this.props.proposal.createdBy.name}</h1>;
