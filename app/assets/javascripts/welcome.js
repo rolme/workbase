4b37d91ece1aedbaa1dd24bb56d66d1bb8387040
Roland Parnaso <roland@sparklab.io>
2016-08-29 21:05:37 -0700

    Install chart via npm.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+import Chart from 'chart.js';
 
 console.log('WORKING!');
 
