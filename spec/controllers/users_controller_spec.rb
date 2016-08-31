3b75fda05916f6a52ed02ae5b4f5792fc1137384
GyanP <gyanig72@gmail.com>
2016-08-31 22:32:13 +0530

    replace old style pendingin rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,15 +12,12 @@ RSpec.describe UsersController, type: :controller do
       expect(user.authenticate('123')).to eql(false)
     end
 
-    pending "does update password only if current password is correct" do
-      # user = FactoryGirl.create(:user1)
-      # session[:current_user_id] = session[:login_user_id] = user.id
-      # post :password_update, params: { slug: user.slug, user: { current_password: '1234', new_pasword: '12345' }}
-      # user = User.find user.slug
-      # # this is just a patch to fix InvalidHash
-      # user.password = '12345'
-      # expect(user.authenticate('12345')).to eql(user)
-      it { should end_with "success" }
+    it "does update password only if current password is correct", :skip => 'unexpected behaviour of BCrypt in rspec' do
+      user = FactoryGirl.create(:user1)
+      session[:current_user_id] = session[:login_user_id] = user.id
+      post :password_update, params: { slug: user.slug, user: { current_password: '1234', new_pasword: '12345' }}
+      user = User.find user.slug
+      expect(user.authenticate('12345')).to eql(user)
     end
   end
 end
