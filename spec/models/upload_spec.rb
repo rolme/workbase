0a7e701968d9a051ee6e4ec70c71943397dea852
GyanP <gyanig72@gmail.com>
2016-09-02 21:52:22 +0530

    spec for upload modal

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Upload, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Should not associate with unit" do
+    unit = FactoryGirl.create(:unit)
+    expect(Upload.associate_with_unit(100, unit.id)).to eql(nil)
+  end
+
+  it "Should associate with unit" do
+    upload = FactoryGirl.create(:upload)
+    unit = FactoryGirl.create(:unit)
+    expect(Upload.associate_with_unit(upload.id, unit.id)).to eql(true)
+  end
 end
