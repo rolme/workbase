5505ca59ac53e0489743087977a5f71bd9dad5ba
Akshay <akshaybabar2408@gmail.com>
2016-09-03 21:33:51 +0530

    Correct indentation and replace eWorkbase

--- app/assets/javascripts/sidebar_menu.js
+++ app/assets/javascripts/sidebar_menu.js
@@ -1,36 +1,31 @@
 $(document).on("click","#menu-toggle",function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
 
-     
- 
-     function initMenu() {
-      $('#menu ul').hide();
-      $('#menu ul').children('.current').parent().show();
-      //$('#menu ul:first').show();
-      $('#menu li a').click(
-        function() {
-          var checkElement = $(this).next();
-          if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-            return false;
-            }
-          if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-            $('#menu ul:visible').slideUp('normal');
-            checkElement.slideDown('normal');
-            return false;
-            }
-          }
-        );
+function initMenu() {
+  $('#menu ul').hide();
+  $('#menu ul').children('.current').parent().show();
+  $('#menu li a').click(
+    function() {
+      var checkElement = $(this).next();
+      if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+        return false;
+        }
+      if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+        $('#menu ul:visible').slideUp('normal');
+        checkElement.slideDown('normal');
+        return false;
+        }
       }
-    $(document).ready(function() {
+    );
+}
 
-      initMenu();
-$("#menu-toggle-2").click(function(e) {
-      
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled-2");
-        $('#menu ul').hide();
-    });
-
-    });
+$(document).ready(function() {
+  initMenu();
+  $("#menu-toggle-2").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled-2");
+    $('#menu ul').hide();
+  });
+});
