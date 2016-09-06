7155108aa8270252bbf8e71ceaa3e36468c7bdad
Roland Parnaso <roland@sparklab.io>
2016-09-06 07:59:01 -0700

    Set config for local sidekiq too.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,7 @@
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: Rails.application.secrets.redis_url }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = { url: Rails.application.secrets.redis_url }
+end
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: Rails.application.secrets.redis_url }
-  end
+Sidekiq.configure_client do |config|
+  config.redis = { url: Rails.application.secrets.redis_url }
 end
