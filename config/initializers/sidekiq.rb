4a3787c4c05156c9ddf6548d2fc80c0144347e95
Roland Parnaso <roland@sparklab.io>
2016-09-06 07:57:32 -0700

    Add configuration file for sidekiq.

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: Rails.application.secrets.redis_url }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: Rails.application.secrets.redis_url }
+  end
+end
