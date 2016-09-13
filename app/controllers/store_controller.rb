840decae221984fbc6364089297e9ec60cb07e87
sean linke <sean.tnl@gmail.com>
2016-09-13 09:18:20 -0700

    Sort the enabled features first

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
 class StoreController < ApplicationController
   def index
     @settings = current_user.company.settings
-    @features = Feature.active.all
+    @features = Feature.active.all.sort_by{ |f| @settings.exists?(f) ? 1 : 0 }
   end
 end
