2c1e1c8d16231d0cc809c1e98697ebf450f52aed
Roland Parnaso <roland@sparklab.io>
2016-09-20 07:29:08 -0700

    Add default status for company.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,8 @@
 class Company < ApplicationRecord
   include SoftDeletable
 
+  belongs_to :company_status
+
   has_many :areas
   has_many :clients
   has_many :locations
@@ -14,9 +16,14 @@ class Company < ApplicationRecord
 
   before_validation(on: :create) do
     default_company_name
+    default_company_status
   end
 
   private
+
+  def default_company_status
+    self.company_status = CompanyStatus.default if company_status_id.blank?
+  end
 
   # assign default company name
   def default_company_name
--- app/models/company_status.rb
+++ app/models/company_status.rb
@@ -1,2 +1,6 @@
 class CompanyStatus < Registry
+  # get default type
+  def self.default
+    find_by(label: 'trial')
+  end
 end
