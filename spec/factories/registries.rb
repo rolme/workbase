99089894f0dada7ac9b2797db38f73488adf5999
Roland Parnaso <roland@sparklab.io>
2016-09-22 13:52:12 -0700

    Quick tweaks for testing.

--- spec/factories/registries.rb
+++ spec/factories/registries.rb
@@ -4,11 +4,4 @@ FactoryGirl.define do
     position "MyString"
     deleted_at "2016-08-03 20:42:06"
   end
-
-  factory :encounter_type do
-    label "MyString"
-    position "MyString"
-    type "EncounterType"
-    deleted_at "2016-08-03 20:42:06"
-  end
 end
