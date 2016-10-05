35698cbbaed8dfc2107d40616f90d6b659563152
sean linke <sean.tnl@gmail.com>
2016-10-04 23:45:33 -0700

    Converts task form to smart component

--- app/assets/javascripts/tasks/components/empty_task_item.js
+++ app/assets/javascripts/tasks/components/empty_task_item.js
@@ -1,20 +1,68 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as TaskActions from '../actions/task_actions';
 
-const EmptyTaskItem = ({ saveTask }) => {
-  return (
-    <tr>
-      <td></td>
-      <td className="task-title"><input /></td>
-      <td className="task-due-date"><input /></td>
-      <td className="task-actions">
-        <i className="fa fa-save" aria-hidden="true" onClick={() => saveTask({ fill: 'me in' })}></i>
-      </td>
-    </tr>
-  );
+class EmptyTaskItem extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      authenticity_token: null,
+    };
+
+    this.saveTask = this.saveTask.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState(
+        Object.assign(
+            {},
+            this.state,
+            { authenticity_token: $('meta[name=csrf-token]').attr('content') }
+        )
+    );
+  }
+
+  saveTask() {
+    // build the task props from the inputs
+    let props = {
+      title:  this.refs.title.value.trim(),
+      due_at: this.refs.due_date.value.trim(),
+      authenticity_token: this.state.authenticity_token,
+    };
+
+    if (props.title === "") return;
+
+    this.props.actions.createTask(props);
+  }
+
+  render() {
+    return (
+      <tr>
+        <td></td>
+        <td className="task-title"><input name="title" ref="title" placeholder="Enter a description" /></td>
+        <td className="task-due-date"><input name="due_date" ref="due_date" placeholder="MM/DD/YYYY" /></td>
+        <td className="task-actions">
+          <i className="fa fa-save" aria-hidden="true" onClick={this.saveTask}></i>
+        </td>
+      </tr>
+    );
+  }
 }
 
 EmptyTaskItem.propTypes = {
-  saveTask: PropTypes.func.isRequired,
 };
 
-export default EmptyTaskItem;
+function mapStateToProps(state, ownProps) {
+  return {
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(TaskActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EmptyTaskItem);
